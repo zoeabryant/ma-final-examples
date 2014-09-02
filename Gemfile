d0add0f72812850f6203a91d027fc93002d98cfd
Zoe Bryant <zoebryant@Zoes-MacBook-Pro.local>
2014-09-02 14:20:14 +0100

    Examples: Comparing Lat/Lng pairs and parsing through JSON

---
+++ Gemfile
@@ -0,0 +1,2 @@
+source "http://www.rubygems.org"
+gem 'geo-distance', '~> 0.2.0'
---
+++ get_steps.rb
@@ -0,0 +1,33 @@
+require 'json'
+
+file = File.read('json/steps.json')
+data_hash = JSON.parse(file)
+
+# puts data_hash[0]['segments'][1]['activities'][0]['trackPoints']
+# puts
+json_strings = data_hash.length
+
+json_strings.times do |json_part|
+
+	segments = data_hash[json_part]['segments']
+
+	segments.each_with_index do |val, i|
+
+		activities = segments[i]['activities']
+
+		if activities
+
+			activities.each_with_index do |val, x|
+
+				track_points = activities[x]['trackPoints']
+
+				puts track_points
+
+			end
+
+		end
+
+		puts
+	end
+
+end
